e38b2ea8432ef6eed04ead16673b25d21ac4346c
erika-vazquez <ito2016@protonmail.ch>
2019-07-19 12:54:46 -0500

    Built component that displays one character

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import CharactersList from './components/CharactersList'
+import axios from 'axios'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [data, setData] = useState({});
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+useEffect(() => {
+  console.log("hello")
+  axios
+  .get('https://swapi.co/api/people/1/')
+  .then(response =>{
+    console.log(response.data)
+    setData(response.data)
+    
+  })
+
+}, []);
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+    <CharactersList name={data.name}
+    gender={data.gender}
+    height={data.height}
+
+    />
     </div>
   );
 }
--- starwars/src/components/CharactersList.js
+++ starwars/src/components/CharactersList.js
@@ -1,1 +1,14 @@
-import react from 'react';
+import React from 'react';
+
+const CharacterList = (props) => {
+    
+    return(
+        <div className="character-card">
+            <h1>Character: </h1>
+            <h2>{props.name}</h2>
+            <h3>{props.gender}</h3>
+            <h3>{props.height}</h3>
+        </div>
+    )
+}
+export default CharacterList 
